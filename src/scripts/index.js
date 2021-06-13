acf191c6f6c3a53616e1945224a3e22027e8c0b8
fadhil <965216@telkom.co.id>
2021-06-13 16:38:34 +0700

    setup figure on card restaurant

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,37 @@
-import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
-import '../styles/responsive.css';
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.css";
+import "../styles/responsive.css";
+import data from "../DATA.json";
 
 const hamburgerBtn = document.querySelector("#hamburger");
 const drawerElement = document.querySelector("#drawer");
 const mainElement = document.querySelector("main");
 
-hamburgerBtn.addEventListener("click", event => {
+hamburgerBtn.addEventListener("click", (event) => {
   drawerElement.classList.toggle("open");
-  console.log(drawerElement.classList)
+  console.log(drawerElement.classList);
   event.stopPropagation();
 });
 
-mainElement.addEventListener("click", event => {
+mainElement.addEventListener("click", (event) => {
   drawerElement.classList.remove("open");
   event.stopPropagation();
-})
+});
+
+let listRestaurant = "";
+data.restaurants.forEach(
+  (item) =>
+    (listRestaurant += `
+    <div class="card">
+      <figure>
+        <img src="${item.pictureId}" alt="suasana restaurant ${item.name}">
+        <span>Rating: <strong>${item.rating}</strong> | Kota: <strong>${item.city}</strong></span>
+      </figure>
+      <article>
+        <p>Name: ${item.name}</p>
+        <p>Description: ${item.description}</p>
+      </article>
+    </div>
+  `)
+);
+document.querySelector("#card-restaurants").innerHTML = listRestaurant;
