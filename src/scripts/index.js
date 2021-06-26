ee51d4d0cea099ce575b0f5f1221ee786328e2ed
fadhil <965216@telkom.co.id>
2021-06-26 12:11:37 +0700

    rename css files

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,6 @@
 import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/rounded-main.css';
-import '../styles/rounded-responsive.css';
+import '../styles/compact-main.css';
+import '../styles/compact-responsive.css';
 import App from './views/app';
 
 const app = new App({
