b48be1ccfbb23a5a06b75049da32c21c9d79fb74
fadhil <965216@telkom.co.id>
2021-07-10 22:21:10 +0700

    setup integration test

---
+++ specs/unfavoriteRestaurantSpec.js
@@ -0,0 +1,51 @@
+import FavoriteRestaurantIdb from '../src/scripts/data/favorite-restaurant-idb';
+import * as TestFactories from './helpers/testFactories';
+
+const addFavoriteButtonContainer = () => {
+  document.body.innerHTML = '<div id="favoriteButtonContainer"></div>';
+};
+
+describe('Unliking A Restaurant', () => {
+  beforeEach(async () => {
+    addFavoriteButtonContainer();
+    await FavoriteRestaurantIdb.putRestaurant({ id: 1 });
+  });
+
+  afterEach(async () => {
+    await FavoriteRestaurantIdb.deleteRestaurant(1);
+  });
+
+  it('should display unfavorite widget when the restaurant has been favorited', async () => {
+    await TestFactories.createFavoriteButtonPresenterWithRestaurant({ id: 1 });
+
+    expect(document.querySelector('[aria-label="hapus restoran ini dari favorit"]')).toBeTruthy();
+  });
+
+  it('should not display favorite widget when the restaurant has been favorited', async () => {
+    await TestFactories.createFavoriteButtonPresenterWithRestaurant({ id: 1 });
+
+    expect(document.querySelector('[aria-label="favoritkan restoran ini"]')).toBeFalsy();
+  });
+
+  it('should be able to remove favorited restaurant from the list', async () => {
+    await TestFactories.createFavoriteButtonPresenterWithRestaurant({ id: 1 });
+
+    document
+      .querySelector('[aria-label="hapus restoran ini dari favorit"]')
+      .dispatchEvent(new Event('click'));
+
+    expect(await FavoriteRestaurantIdb.getAllRestaurants()).toEqual([]);
+  });
+
+  it('should not throw error if the unfavorited restaurant is not in the list', async () => {
+    await TestFactories.createFavoriteButtonPresenterWithRestaurant({ id: 1 });
+
+    await FavoriteRestaurantIdb.deleteRestaurant(1);
+
+    document
+      .querySelector('[aria-label="hapus restoran ini dari favorit"]')
+      .dispatchEvent(new Event('click'));
+
+    expect(await FavoriteRestaurantIdb.getAllRestaurants()).toEqual([]);
+  });
+});
